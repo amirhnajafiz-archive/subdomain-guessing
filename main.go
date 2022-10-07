af30a36b0bf86a77277a3701439b7871dce38442
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-07 16:28:15 +0330

    replace: interface file

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/amirhnajafiz/subdomain-guessing/pkg"
+)
+
+// Lookup
+// manages 3 methods for lookup types.
+type Lookup interface {
+	// TypeA ns lookup.
+	TypeA(fqdn, serverAddr string) ([]string, error)
+	// TypeCNAME ns lookup.
+	TypeCNAME(fqdn, serverAddr string) ([]string, error)
+	// Navigate ns lookup.
+	Navigate(fqdn, serverAddr string) []pkg.Result
+}
 
 func main() {
 	fmt.Println("vim-go")
