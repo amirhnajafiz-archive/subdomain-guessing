9c62cefe4c7b61d163a2b20fc2174db2ed4049cc
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-07 16:31:51 +0330

    add: application struct

--- main.go
+++ main.go
@@ -2,20 +2,7 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/amirhnajafiz/subdomain-guessing/pkg"
 )
-
-// Lookup
-// manages 3 methods for lookup types.
-type Lookup interface {
-	// TypeA ns lookup.
-	TypeA(fqdn, serverAddr string) ([]string, error)
-	// TypeCNAME ns lookup.
-	TypeCNAME(fqdn, serverAddr string) ([]string, error)
-	// Navigate ns lookup.
-	Navigate(fqdn, serverAddr string) []pkg.Result
-}
 
 func main() {
 	fmt.Println("vim-go")
--- pkg/lookup.go
+++ pkg/lookup.go
@@ -1,1 +1,30 @@
 package pkg
+
+// Lookup
+// manages 3 methods for lookup types.
+type Lookup interface {
+	// TypeA ns lookup.
+	TypeA(fqdn, serverAddr string) ([]string, error)
+	// TypeCNAME ns lookup.
+	TypeCNAME(fqdn, serverAddr string) ([]string, error)
+	// Navigate ns lookup.
+	Navigate(fqdn, serverAddr string) []Result
+}
+
+type application struct{}
+
+func NewApp() Lookup {
+	return &application{}
+}
+
+func (a *application) TypeA(fqdn, serverAddr string) ([]string, error) {
+	return nil, nil
+}
+
+func (a *application) TypeCNAME(fqdn, serverAddr string) ([]string, error) {
+	return nil, nil
+}
+
+func (a *application) Navigate(fqdn, serverAddr string) []Result {
+	return nil
+}
