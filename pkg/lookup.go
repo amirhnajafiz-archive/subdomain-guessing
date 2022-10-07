e223746c9f97f5e8c0c4de1cbfc8d6c01b762bd4
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-07 16:28:03 +0330

    replace: interface file

--- pkg/lookup.go
+++ pkg/lookup.go
@@ -1,12 +1,1 @@
 package pkg
-
-// Lookup
-// manages 3 methods for lookup types.
-type Lookup interface {
-	// TypeA ns lookup.
-	TypeA(fqdn, serverAddr string) ([]string, error)
-	// TypeCNAME ns lookup.
-	TypeCNAME(fqdn, serverAddr string) ([]string, error)
-	// Navigate ns lookup.
-	Navigate(fqdn, serverAddr string) []Result
-}
